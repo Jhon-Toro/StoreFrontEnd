41e1131fc8da8941b01568dec2645a7de77dd40d
Jhon-Toro <toromurieljhon@gmail.com>
2024-11-16 15:51:57 -0500

    Fix to build

--- src/pages/UserOrders/UserOrdersPage.tsx
+++ src/pages/UserOrders/UserOrdersPage.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from "react";
-import axiosInstance from "../../api/axiosIntance";
-import { useSelector } from "react-redux";
-import { RootState } from "../../store";
-import "./UserOrdersPage.scss";
-import Pagination from "../../components/shared-components/pagination/Pagination";
+import React, { useEffect, useState } from 'react';
+import axiosInstance from '../../api/axiosIntance';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
+import './UserOrdersPage.scss';
 
 interface Order {
     id: number;
@@ -15,24 +14,20 @@ interface Order {
 
 const UserOrdersPage: React.FC = () => {
     const [orders, setOrders] = useState<Order[]>([]);
-    const [currentPage, setCurrentPage] = useState(1);
+    const [currentPage] = useState(1);
     const [ordersPerPage] = useState(5);
     const token = useSelector((state: RootState) => state.auth.token);
 
     useEffect(() => {
         const fetchOrders = async () => {
             try {
-                const response = await axiosInstance.get("/orders/my_orders", {
+                const response = await axiosInstance.get('/orders/my_orders', {
                     headers: { Authorization: `Bearer ${token}` },
                 });
-                const sortedOrders = response.data.sort(
-                    (a: Order, b: Order) =>
-                        new Date(b.created_at).getTime() -
-                        new Date(a.created_at).getTime()
-                );
+                const sortedOrders = response.data.sort((a: Order, b: Order) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
                 setOrders(sortedOrders);
             } catch (error) {
-                console.error("Error fetching orders:", error);
+                console.error('Error fetching orders:', error);
             }
         };
         fetchOrders();
@@ -42,14 +37,8 @@ const UserOrdersPage: React.FC = () => {
     const indexOfFirstOrder = indexOfLastOrder - ordersPerPage;
     const currentOrders = orders.slice(indexOfFirstOrder, indexOfLastOrder);
 
-    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
-
     return (
-        <div
-            className={`user-orders-page ${
-                currentOrders.length < 1 ? "currentOrders-empty" : ""
-            }`}
-        >
+        <div className={`user-orders-page ${currentOrders.length < 1 ? 'currentOrders-empty' : ''}`}>
             <h1 className="page-title">Mis Órdenes</h1>
             <ul className="orders-list">
                 {currentOrders.length > 0 ? (
@@ -57,40 +46,31 @@ const UserOrdersPage: React.FC = () => {
                         <li key={order.id} className="order-card">
                             <div className="order-header">
                                 <h2>Orden ID: {order.id}</h2>
-                                <p className="order-date">
-                                    {new Date(
-                                        order.created_at
-                                    ).toLocaleDateString()}
-                                </p>
+                                <p className="order-date">{new Date(order.created_at).toLocaleDateString()}</p>
                             </div>
                             <div className="order-details">
                                 <p>
-                                    <span>Total:</span> $
-                                    {order.total_price.toFixed(2)}
+                                    <span>Total:</span> ${order.total_price.toFixed(2)}
                                 </p>
                                 <p>
-                                    <span>Estado de la Orden:</span>{" "}
-                                    {order.order_status}
+                                    <span>Estado de la Orden:</span> {order.order_status}
                                 </p>
                                 <p>
-                                    <span>Estado de Pago:</span>{" "}
-                                    {order.payment_status}
+                                    <span>Estado de Pago:</span> {order.payment_status}
                                 </p>
                             </div>
                         </li>
                     ))
                 ) : (
-                    <p className="no-orders">
-                        Aún no tienes órdenes realizadas.
-                    </p>
+                    <p className="no-orders">Aún no tienes órdenes realizadas.</p>
                 )}
             </ul>
-            <Pagination
+            {/* <Pagination
                 ordersPerPage={ordersPerPage}
                 totalOrders={orders.length}
                 paginate={paginate}
                 currentPage={currentPage}
-            />
+            /> */}
         </div>
     );
 };
